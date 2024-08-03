9a37d3a698e8157279eaa507f5f94b8245a2872a
ananijoel <ananijoellanatide@gmail.com>
2024-08-03 20:38:07 +0000

    feat:activation de la base de donnee

--- src/dataBase/sequelize.js
+++ src/dataBase/sequelize.js
@@ -6,7 +6,7 @@ mock_items = require('./mock')
 let sequelize
 if(process.env.NODE_ENV === 'production'){
      sequelize = new Sequelize('afpqk027nzhceqmm', 'gw6fhtniqf49dc4s', 'jy4ja78q9bikzu1d', {
-        host: '	nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+        host: 'nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
         dialect: 'mariadb',
         dialectOptions: {
           timezone: 'Etc/GMT',
@@ -29,7 +29,7 @@ const item = item_model(sequelize,DataTypes)
 const user = user_model(sequelize,DataTypes)
 const init_dataBase = () => {
     return sequelize.sync(
-      //{force:true}
+      {force:true}
     ).then(() => {
       mock_items.map(itemss =>{
         item.create({

903c125e6b130523e89b7f37cf2d7d3ff4b9fde9
ananijoel <ananijoellanatide@gmail.com>
2024-08-05 08:18:30 +0000

    fix: retrait du favicon

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/dataBase/sequelize')
 const cors = require('cors')
@@ -11,7 +10,7 @@ const port = process.env.PORT || staticport
 sequelize.init_dataBase()
 
 app
-    .use(bodyParser.json())// middleware qui sert a parser toutes les entres de la web app du format string au format json
+    .use(bodyParser.json())
     .use(cors())
 
 
